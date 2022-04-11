ded66094e21a1a65de94903e13589723a89efa9b
olalekan <altechbusinesses@gmail.com>
2022-04-11 07:47:50 +0100

    removed all localhosts

--- client/src/graphqlClient.js
+++ client/src/graphqlClient.js
@@ -1,6 +1,6 @@
 import { GraphQLClient } from "graphql-request";
 
-const endpoint = "http://localhost:5000/graphql";
+const endpoint = "/graphql";
 const client = new GraphQLClient(endpoint);
 
 export default client;
